952e41883d5020405e36686710bea00f4f661794
parkgunwook1 <pkw85428@naver.com>
2023-09-08 14:02:05 +0900

    ch3

---
+++ src/ch3/MathBean.java
@@ -0,0 +1,16 @@
+package ch3;
+
+public class MathBean {
+    public void printClassName() {
+        System.out.println("MathBean");
+    }
+    public void printNumber(int number) {
+        System.out.println(number);
+    }
+    public int getOne() {
+        return 1;
+    }
+    public int plus(int x,int y) {
+        return x + y;
+    }
+}
---
+++ src/ch3/VendingMachineMain.java
@@ -0,0 +1,12 @@
+package ch3;
+
+public class VendingMachineMain {
+    public static void main(String[]args) {
+        VendingMachine.printVersion();
+        VendingMachine vm1 = new VendingMachine();
+        VendingMachine vm2 = new VendingMachine();
+
+        String product = vm1.pushProductButton(100);
+        System.out.println(product);
+    }
+}
