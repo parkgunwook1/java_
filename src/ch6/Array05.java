cd32defb21a9521aad4578e738a4765888622f08
parkgunwook1 <pkw85428@naver.com>
2023-09-14 13:04:55 +0900

    ch5 array

---
+++ src/ch6/Array05.java
@@ -0,0 +1,14 @@
+package ch6;
+
+public class Array05 {
+    public static void main(String[] args) {
+        int[][] array = {{0,1},{2,3,4}};
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/ch6/Array06.java
@@ -0,0 +1,11 @@
+package ch6;
+
+public class Array06 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5};
+
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
