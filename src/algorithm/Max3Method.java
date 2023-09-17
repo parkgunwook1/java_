1c28e58fead1e4d668cf03173b5ac86fa3511470
parkgunwook1 <pkw85428@naver.com>
2023-09-17 21:53:24 +0900

    자료구조

---
+++ src/algorithm/Max3Method.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+public class Max3Method {
+
+    static int max3(int a, int b, int c) {
+        int max = a;
+        if(b > max)
+            max = b;
+        if (c > max)
+            max = c;
+
+        return max;
+        }
+}
