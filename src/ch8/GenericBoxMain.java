83cce74fd1018c420e37ffd7b3c70acd058f0a09
parkgunwook1 <pkw85428@naver.com>
2023-09-18 11:30:35 +0900

    generic

---
+++ src/ch8/GenericBoxMain.java
@@ -0,0 +1,10 @@
+package ch8;
+
+public class GenericBoxMain {
+    public static void main(String[] args) {
+        GenericBox<String> genericBox =  new GenericBox<>();  // 정해지지 않은 타입을 String 으로 사용하겠다.
+        genericBox.add("park");
+        String str = genericBox.get();
+        System.out.println(str.toUpperCase());
+    }
+}
