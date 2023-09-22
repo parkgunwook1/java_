2962ed335630f7e194f05b0a4da0e6421787dded
parkgunwook1 <pkw85428@naver.com>
2023-09-22 14:34:04 +0900

    algorithm

---
+++ src/Study/algorithm/ch1/Square.java
@@ -0,0 +1,21 @@
+package Study.algorithm.ch1;
+
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+
+        System.out.print("사각형을 만들 숫자를 기입하시오 : ");
+
+        Scanner stdIn = new Scanner(System.in);
+        int n = stdIn.nextInt();
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
