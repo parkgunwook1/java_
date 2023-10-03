3049dd8bd7c21368547577930a7c7cbdeda00896
parkgunwook1 <pkw85428@naver.com>
2023-10-03 18:48:28 +0900

    doit_java

---
+++ src/school/alone/ch_1/exception/ArrayExceptionHandling.java
@@ -0,0 +1,18 @@
+package school.alone.ch_1.exception;
+
+public class ArrayExceptionHandling {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        try {
+            for(int i = 0; i <= 5; i++) {  // 예외가 발생할 수 있으므로 try 블록에 작성
+                arr[i] =i;
+                System.out.println(arr[i]);
+            }
+        }catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);                 // 예외가 발생하면 catch 블록 수행
+            System.out.println("예외 처리 부분");
+        }
+        System.out.println("프로그램 종료");
+    }
+}
