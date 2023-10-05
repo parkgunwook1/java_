0e0b43177b39a91e792de1183a210c3cc3a39d6e
parkgunwook1 <pkw85428@naver.com>
2023-10-05 10:28:17 +0900

    doit_java

---
+++ src/school/alone/ch_1/array/arraylist/Sample.java
@@ -0,0 +1,17 @@
+package school.alone.ch_1.array.arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Sample {
+    // 자바에서 Array를 List로 변환하기 위해서는 Arrays.asList(array)를 사용한다.
+    public static void main(String[] args) {
+        ArrayList<String> pitches = new ArrayList<>(Arrays.asList("138","129","142"));
+        pitches.sort(Comparator.naturalOrder());
+        // 오름차순 정렬 - Comparator.naturalOrder()
+        // 내림찬순 정렬 - Comparator.reverseOrder()
+        // 정렬하기 위해서는 sort 메서드를 사용해야 함.
+        System.out.println(pitches);
+    }
+}
---
+++ src/school/ch1/together/study/control/run/Run.java
@@ -0,0 +1,10 @@
+package school.ch1.together.study.control.run;
+
+import school.ch1.together.study.control.A_if1;
+
+public class Run {
+    public static void main(String[] args) {
+        A_if1 a1 = new A_if1();
+        a1.method5();
+    }
+}
